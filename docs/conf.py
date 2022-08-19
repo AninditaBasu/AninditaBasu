b6fb565a4a8b6ec39c7f13261a8cb9858b7fcd5e
anindita <ab.techwriter@gmail.com>
2022-08-19 09:50:51 +0530

    awesome theme nav

--- docs/conf.py
+++ docs/conf.py
@@ -151,8 +151,8 @@ html_permalinks_icon = '<span>#</span>'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
-# documentation.
-#html_theme_options = {}
+# documentation at https://sphinxawesome.xyz/.
+#html_theme_options = {"show_scrolltop": True}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
